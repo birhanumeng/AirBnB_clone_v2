bbed065e413d73098f7c29789772deeede91a535
= <=>
2023-02-06 09:00:01 +0000

    render template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,7 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbers_templates(n):
+def numbersandtemplates(n):
     """Dispalys a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
